a5a75eb14c0a26b374ac2f6cb532f9677a8b8711
KangHyun Jang <khjang4848@gmail.com>
2017-08-21 18:47:34 +0900

    FrontEnd Code Testing

--- test/NewInvestmentViewSpec.js
+++ test/NewInvestmentViewSpec.js
@@ -1,7 +1,19 @@
 describe("NewInvestmentView는", () => {
+    let view;
     beforeEach(() => {
+        jasmine.getFixtures().fixturesPath = "base/test/fixtures";
         loadFixtures("NewInvestmentView.html");
+
+        view = new NewInvestmentView({selector : '#new-investment'});
     });
+
+    it("DOM 앨리먼트를 프로퍼티로 노출해야한다", () => {
+        expect(view.$el).toExist();
+    });
+
+    it("빈 종목 코드를 가지고 있어야 한다", () => {
+        expect(view.getSymbolInput()).toHaveValue("");
+    })
 
     it("종목 코드를 입력받어야 한다", () => {
 
